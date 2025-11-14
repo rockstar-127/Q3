d52f1b08334440f8c7e72f6e3b353fa4ba9531d4
Rinkesh Verma <225209588+rockstar-127@users.noreply.github.com>
2025-11-15 03:33:23 +0530

    SelectionSort implemented

--- Sorting_Package/src/sorting_algorithms.py
+++ Sorting_Package/src/sorting_algorithms.py
@@ -19,3 +19,20 @@ class BubbleSort(SortAlgorithm):
                     data[j], data[j+1] = data[j+1], data[j]
 
         return data
+
+
+class SelectionSort(SortAlgorithm):
+    def sort(self, data, order="ascending"):
+        data = data.copy()
+        n = len(data)
+
+        for i in range(n):
+            idx = i
+            for j in range(i + 1, n):
+                if (order == "ascending" and data[j] < data[idx]) or \
+                   (order == "descending" and data[j] > data[idx]):
+                    idx = j
+
+            data[i], data[idx] = data[idx], data[i]
+
+        return data
