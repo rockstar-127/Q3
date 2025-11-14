921453257c2225063f0aeb18e2193b0b201556ef
Rinkesh Verma <225209588+rockstar-127@users.noreply.github.com>
2025-11-15 03:31:53 +0530

    bubbleSort implemented

--- Sorting_Package/src/sorting_algorithms.py
+++ Sorting_Package/src/sorting_algorithms.py
@@ -4,3 +4,18 @@ class SortAlgorithm(ABC):
     @abstractmethod
     def sort(self, data, order="ascending"):
         pass
+
+
+class BubbleSort(SortAlgorithm):
+
+    def sort(self, data, order="ascending"):
+        data = data.copy()
+        n = len(data)
+
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if (order == "ascending" and data[j] > data[j+1]) or \
+                   (order == "descending" and data[j] < data[j+1]):
+                    data[j], data[j+1] = data[j+1], data[j]
+
+        return data
